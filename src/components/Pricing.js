972be9b8c5a25aa52088977fd9c999ebbdefb474
Priyank Verma <priyankverma668@gmail.com>
2023-02-09 22:23:52 +0530

    new blog project added

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -9,29 +9,35 @@ const Pricing = () => {
     <div className="card-container">
     
     <div className="card">
-    <h3>- Languages -</h3>
+    <h3>- Languages - </h3>
+    
     <span className="bar"></span>
+    
     <p className="">- HTML -</p>
     <p>- CSS -</p>
     <p>- JavaScript -</p>
     <p>- TypeScript -</p>
-    <p>- TYPE -</p>
+    <p>- SQL -</p>
+    <p>- EJS -</p>
     <Link to="/contact" className="btn">Know More.</Link>
     </div>
     <div className="card">
-    <h3>- Framework and Libraries -</h3>
+    <h3>- Framework  and Libraries  -</h3>
     <span className="bar"></span>
     <p className="">- React.Js -</p>
     <p>- Redux -</p>
     <p>- jQuery -</p>
     <p>- Bootstrap -</p>
     <p>- Express.js -</p>
+   <p>&nbsp;</p>
     <Link to="/contact" className="btn">Know More</Link>
     </div>
     <div className="card">
-    <h3>- Other Skills -</h3>
+    <h3>- Other Skills and DB-</h3>
+    
     <span className="bar"></span>
     <p className="">- Firebase -</p>
+    <p>- MongoDB -</p>
     <p>- Wordpress -</p>
     <p>- Version Control(Git) -</p>
     <p>- Netlify -</p>
--- src/components/work-card-data.js
+++ src/components/work-card-data.js
@@ -1,14 +1,30 @@
 import pro1 from "../assests/pro1.jpeg"
 import pro2 from "../assests/pro2.jpeg"
 import pro5 from "../assests/pro5.jpeg"
+import pro7 from "../assests/pro7.jpeg"
 const ProjectCardData =[
 
     {
         imgsrc : pro1,
         title:"E-Commerce Website",
-        text:"It is A E-Commerce Website in which i provided cart , navigation ,sign-in ,sign-out and firebase and also setup a payment gateway and deployed on netlify",
+        text:"It is A E-Commerce Website in which i provided cart , navigation ,sign-in ,sign-out and firebase and also setup a payment gateway and deployed on netlify.",
         View :"https://my-crown-clothing-shop.netlify.app/",
         source:"https://github.com/verma-priyank/crown-clothing"
+    },
+    {
+        imgsrc : pro7,
+        title:"My Openion Blog",
+        text:"This is a Public personal blog Website. Add /compose in the url for hidden compose page and create your own blog post.And also the database of this app has been hosted on mongoDB.",
+        View :"https://blog-8vwt.onrender.com",
+        source:"https://github.com/verma-priyank/Blog"
+    },
+   
+    {
+        imgsrc : pro5,
+        title:"Tindog Website",
+        text:"It is a Social Media Website. Just like tinder You can also use Tindog for your pets.This website is fully Responsive. &nbsp;&nbsp;",
+        View :"https://verma-priyank.github.io/tindog-Website/",
+        source:"https://github.com/verma-priyank/tindog-Website"
     },
     {
         imgsrc : pro2,
@@ -16,13 +32,6 @@ const ProjectCardData =[
         text:"It is a UI interface using React.In which we got our Cards from API.In this Project Bootstrap and Typescript is also used .                 ",
         View :"https://coruscating-meerkat-624731.netlify.app/",
         source:"https://github.com/verma-priyank/Monster-Rolodex"
-    },
-    {
-        imgsrc : pro5,
-        title:"Tindog Website",
-        text:"It is a Social Media Website. Just like tinder You can also use Tindog for your pets.This website is fully Responsive.",
-        View :"https://verma-priyank.github.io/tindog-Website/",
-        source:"https://github.com/verma-priyank/tindog-Website"
     }
 ]
 export default ProjectCardData;
